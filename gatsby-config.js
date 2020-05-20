3db0c1cc5265423afd84ab3309f01f6152efeeaa
TheNomanAhmed <nomanahmed478@gmail.com>
2020-05-20 09:08:17 +0300

    wip: Added Projects component and Page query from Strapi CMS

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,7 +32,7 @@ module.exports = {
         queryLimit: 1000, // Default to 100
         // contentTypes: [`jobs`, `projects`, `blogs`, ],
         //singleTypes:[`about` ]
-        contentTypes: [`jobs`],
+        contentTypes: [`jobs`, `projects`],
         // singleTypes: [`about`],
       },
     },
