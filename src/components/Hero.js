8cb034fe6bbc3390b6caf611db432b56a2dceb64
TheNomanAhmed <nomanahmed478@gmail.com>
2020-05-20 04:08:41 +0300

    wip: Added Hero Section

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,10 +3,42 @@ import Image from "gatsby-image"
 import { Link } from "gatsby"
 import { graphql, useStaticQuery } from "gatsby"
 import SocialLinks from "../constants/socialLinks"
-// ...GatsbyImageSharpFluid
 
 const Hero = () => {
-  return <h2>hero component</h2>
+  const {
+    file: {
+      childImageSharp: { fluid },
+    },
+  } = useStaticQuery(graphql`
+    {
+      file(relativePath: { eq: "hero-img.png" }) {
+        childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `)
+
+  return (
+    <header className="hero">
+      <div className="section-center hero-center">
+        <article className="hero-info">
+          <div>
+            <div className="underline"></div>
+            <h2>Hello👋 I'm Noman</h2>
+            <h4>I am a freelance developer based in Finland</h4>
+            <Link to="/contact" className="btn">
+              Contact me
+            </Link>
+            <SocialLinks />
+          </div>
+        </article>
+        <Image fluid={fluid} className="hero-img" />
+      </div>
+    </header>
+  )
 }
 
 export default Hero
--- src/constants/socialLinks.js
+++ src/constants/socialLinks.js
@@ -11,27 +11,27 @@ const data = [
   {
     id: 1,
     icon: <FaFacebookSquare className="social-icon"></FaFacebookSquare>,
-    url: "https://www.twitter.com",
+    url: "https://www.facebook.com/TheNomanAhmed",
   },
   {
     id: 2,
     icon: <FaLinkedin className="social-icon"></FaLinkedin>,
-    url: "https://www.twitter.com",
+    url: "https://www.linkedin.com/in/TheNomanAhmed",
   },
   {
     id: 3,
     icon: <FaDribbbleSquare className="social-icon"></FaDribbbleSquare>,
-    url: "https://www.twitter.com",
+    url: "https://www.dribbble.com/TheNomanAhmed",
   },
   {
     id: 4,
     icon: <FaBehanceSquare className="social-icon"></FaBehanceSquare>,
-    url: "https://www.twitter.com",
+    url: "https://www.behance.com/TheNomanAhmed",
   },
   {
     id: 5,
     icon: <FaTwitterSquare className="social-icon"></FaTwitterSquare>,
-    url: "https://www.twitter.com",
+    url: "https://www.twitter.com/TheNomanAhmed",
   },
 ]
 const links = data.map(link => {
