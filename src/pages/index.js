174495b8f9872976864e53a8c4bf41d4a2e34d3d
TheNomanAhmed <nomanahmed478@gmail.com>
2020-05-20 09:42:00 +0300

    wip: Added Blog component and Created dynamic blog pages using Gatsby node

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,6 +9,7 @@ import Blogs from "../components/Blogs"
 export default ({ data }) => {
   const {
     allStrapiProjects: { nodes: projects },
+    allStrapiBlogs: { nodes: blogs },
   } = data
   return (
     <Layout>
@@ -16,6 +17,7 @@ export default ({ data }) => {
       <Services />
       <Jobs />
       <Projects projects={projects} title="Featured Projects" showLink />
+      <Blogs blogs={blogs} title="Latest Articles" showLink />
     </Layout>
   )
 }
@@ -39,6 +41,22 @@ export const query = graphql`
         stack {
           id
           title
+        }
+      }
+    }
+    allStrapiBlogs(sort: { fields: date, order: DESC }, limit: 3) {
+      nodes {
+        category
+        slug
+        date(formatString: "MMMM Do,YYYY")
+        id
+        title
+        image {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid
+            }
+          }
         }
       }
     }
