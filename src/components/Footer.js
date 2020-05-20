9ef657c6ed8f7466a18c6f2d4fe411885576727f
TheNomanAhmed <nomanahmed478@gmail.com>
2020-05-20 10:46:11 +0300

    wip: Added SideBar

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,15 @@
 import React from "react"
 import SocialLinks from "../constants/socialLinks"
 const Footer = () => {
-  return <h2>footer component</h2>
+  return (
+    <footer className="footer">
+      <div>
+        <SocialLinks styleClass="footer-links"></SocialLinks>
+        <h5>copyright&copy;{new Date().getFullYear()}</h5>
+        <span>Noman Ahmed</span> All rights reserved
+      </div>
+    </footer>
+  )
 }
 
 export default Footer
